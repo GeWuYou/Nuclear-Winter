c2aab7567012fa52233c006182c7cb619ffc6ccd
Touzi <zkq739617163@163.com>
2025-12-06 20:34:35 +0800

    主场景骨架

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/main_scene/main_scene.tscn")
 
 
 func _on_setting_pressed() -> void:
